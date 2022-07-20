63ec8951e0566598e68afc77d91341ba4c22e5a8
jfranklin12 <j.franks725@gmail.com>
2022-07-19 20:32:54 -0400

    questions added for each class and functions created to ask questions

--- index.js
+++ index.js
@@ -4,28 +4,32 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-function addManager() {
-    inquirer
-    .prompt([
+let team = [];
+
+const manager = [
     {
         type: 'input',
         name: 'manName',
         message: `What is your team manager's name?`,
+        default: `Nick`
     },
     {
         type: 'input',
         name: 'manId',
         message: `What is your team manager's id?`,
+        default: `6969`
     },
     {
         type: 'input',
         name: 'manEmail',
         message: `What is your team manager's email?`,
+        default: `nick@yahoo.com`
     },
     {
         type: 'input',
         name: 'manOffNum',
         message: `What is your team manager's office number?`,
+        default: `1`
     },
     {
         type: 'list',
@@ -33,31 +37,32 @@ function addManager() {
         message: 'Which type of team member would you like to add?',
         choices: [`Engineer`, `Intern`, `I don't want to want to add any more team members`],
     },
-    ]);
-};
+]
 
-function addEngineer() {
-    inquirer
-    .prompt([
+const engineer = [
     {
         type: 'input',
         name: 'engName',
         message: `What is your engineer's name?`,
+        default: `Jules`
     },
     {
         type: 'input',
         name: 'engId',
         message: `What is your engineer's id?`,
+        default: `421`
     },
     {
         type: 'input',
         name: 'engEmail',
         message: `What is your engineer's email?`,
+        default: `jules@gmail.com`
     },
     {
         type: 'input',
         name: 'engGithub',
-        message: `What is your engineer's email?`,
+        message: `What is your engineer's github?`,
+        default: `jfranks`
     },
     {
         type: 'list',
@@ -65,31 +70,32 @@ function addEngineer() {
         message: 'Which type of team member would you like to add?',
         choices: [`Engineer`, `Intern`, `I don't want to want to add any more team members`],
     },
-])
-};
+]
 
-function addIntern() {
-    inquirer
-    .prompt([
+const intern = [
     {
         type: 'input',
         name: 'intName',
         message: `What is your intern's name?`,
+        default: `Lee`
     },
     {
         type: 'input',
         name: 'intId',
         message: `What is your intern's id?`,
+        default: `864`
     },
     {
         type: 'input',
         name: 'intEmail',
         message: `What is your intern's email?`,
+        default: `lee@aol.com`
     },
     {
         type: 'input',
         name: 'intSchool',
         message: `What is your intern's school?`,
+        default: `Georgia Tech`
     },
     {
         type: 'list',
@@ -97,9 +103,55 @@ function addIntern() {
         message: 'Which type of team member would you like to add?',
         choices: [`Engineer`, `Intern`, `I don't want to want to add any more team members`],
     },
-])
+]
+
+function addManager() {
+    inquirer
+    .prompt(manager).then(manRes => {
+            const manager = new Manager(manRes.manName, manRes.manId, manRes.manEmail, manRes.manOffNum);
+            console.log(manager);
+
+            if (manRes.memberType === `Engineer`) {
+                addEngineer();
+            } else if (manRes.memberType === `Intern`) {
+                addIntern();
+            } else if (manRes.memberType === `I don't want to want to add any more team members`) {
+                console.log('We built our dream team!')
+            };
+        });
 };
 
+
+function addEngineer() {
+    inquirer
+        .prompt(engineer).then(engRes => {
+            const engineer = new Engineer(engRes.engName, engRes.engId, engRes.engEmail, engRes.engGithub);
+            console.log(engineer);
+
+            if (engRes.memberType === `Engineer`) {
+                addEngineer();
+            } else if (engRes.memberType === `Intern`) {
+                addIntern();
+            } else if (engRes.memberType === `I don't want to want to add any more team members`) {
+                console.log('We built our dream team!')
+            };
+        });
+};
+
+function addIntern() {
+    inquirer
+        .prompt(intern).then(intRes => {
+            const intern = new Intern(intRes.intName, intRes.intId, intRes.intEmail, intRes.intSchool);
+            console.log(intern);
+
+            if (intRes.memberType === `Engineer`) {
+                addEngineer();
+            } else if (intRes.memberType === `Intern`) {
+                addIntern();
+            } else if (intRes.memberType === `I don't want to want to add any more team members`) {
+                console.log('We built our dream team!')
+            };
+        });
+}
+
 addManager();
-addEngineer();
-addIntern();
